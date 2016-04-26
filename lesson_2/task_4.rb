1d3c8db2b4337a493020e8c874ce044471d19be3
gabriel <pedrogabriel@mail.ru>
2016-04-26 09:42:08 +0300

    Correct 4, lesson 2

--- lesson_2/task_4.rb
+++ lesson_2/task_4.rb
@@ -3,11 +3,9 @@ def alphabet
   dic = ["a", "e", "i", "o", "u", "y"]
   letters = ("a".."z").to_a
   letters.each_with_index do |item, index|
-    dic.each do |i|
-      if i.include?(item)
-        hash[i] = index + 1
-      end
-    end
+      if dic.include?(item)
+        hash[item] = index + 1
+      end    
   end
 
   puts hash.inspect
