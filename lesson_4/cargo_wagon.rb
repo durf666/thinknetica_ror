deb8bebe8ca1a9c31e2523ea870e5040eec85a95
gabriel <pedrogabriel@mail.ru>
2016-05-03 21:31:37 +0300

    correct 3, lesson 4

--- lesson_4/cargo_wagon.rb
+++ lesson_4/cargo_wagon.rb
@@ -1,2 +1,17 @@
 class CargoWagon < Wagon
+  def new(train)
+    if train.class == CargoTrain
+      super
+    else
+      puts "This kind of wagon isn't for this train"
+    end
+  end
+
+  def destroy(train)
+    if train.class == CargoTrain
+      super
+    else
+      puts "This kind of wagon isn't for this train"
+    end
+  end
 end
--- lesson_4/passenger_wagon.rb
+++ lesson_4/passenger_wagon.rb
@@ -1,2 +1,17 @@
 class PassengerWagon < Wagon
+  def new(train)
+    if train.class == PassengerTrain
+      super
+    else
+      puts "This kind of wagon isn't for this train"
+    end
+  end
+
+  def destroy(train)
+    if train.class == PassengerTrain
+      super
+    else
+      puts "This kind of wagon isn't for this train"
+    end
+  end
 end
